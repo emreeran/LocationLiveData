4b6d04f4a73d463601c1765b4d7c274893d3429c
Emre Eran <emre.eran@gmail.com>
2018-09-01 16:03:36 +0300

    Created common view model for observing location

---
+++ sample/src/main/java/com/emreeran/locationlivedata/sample/LocationViewModel.kt
@@ -0,0 +1,22 @@
+package com.emreeran.locationlivedata.sample
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.emreeran.locationlivedata.LocationLiveData
+import com.google.android.gms.location.LocationRequest
+
+/**
+ * Created by Emre Eran on 31.08.2018.
+ */
+class LocationViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var locationLiveData: LocationLiveData = LocationLiveData.create(
+            application,
+            interval = 100,
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    )
+
+    fun getLocation(): LocationLiveData {
+        return locationLiveData
+    }
+}
